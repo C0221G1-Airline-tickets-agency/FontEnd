0f3f45e73537a974376030bee6cba24b00e1ce10
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-09-10 11:50:06 +0700

    fix html/css item passenger

--- airline-tickets-agency-management/src/app/client/component/home/home.component.ts
+++ airline-tickets-agency-management/src/app/client/component/home/home.component.ts
@@ -21,8 +21,8 @@ export class HomeComponent implements OnInit {
   // information Ticket
   locationTo = '';
   locationFrom = '';
-  departureTime = '';
-  endTime = '';
+  startDay = '';
+  endDay = '';
   adults = 1;
   children = 0;
   baby = 0;
@@ -39,136 +39,125 @@ export class HomeComponent implements OnInit {
     this.getDayNow();
     this.getAllLocation();
     this.getTop10cheapestFlights();
-    this.departureTime = this.dayNow;
+    this.startDay = this.dayNow;
   }
 
-  //#region effect ticket
-  getDayNow() {
-    this.dayNow = formatDate(new Date(), 'yyyy-MM-dd', 'en_US');
-  }
+    //#region effect ticket
+    getDayNow() {
+      this.dayNow = formatDate(new Date(), 'yyyy-MM-dd', 'en_US');
+    }
 
-  clickHidden(num: number) {
-    this.ticketShow = document.getElementsByClassName('showHidden');
-    // ------------------------------ Click HIDDEN / SHOW ---------------------------------------
-    if (num !== 3) {
-      if (this.ticketShow[num].style.display === 'block') {
-        this.ticketShow[num].style.display = 'none';
+    clickHidden(num: number) {
+      this.ticketShow = document.getElementsByClassName('showHidden');
+      // ------------------------------ Click HIDDEN / SHOW ---------------------------------------
+      if (num !== 3) {
+        if (this.ticketShow[num].style.display === 'block') {
+          this.ticketShow[num].style.display = 'none';
+          return;
+        }
+      }
+      // ------------------------------ Click NEXT SHOW ---------------------------------------
+      if (num === 3) {
+        this.ticketShow[(num - 1)].style.display = 'none';
         return;
       }
+      this.ticketShow[this.index].style.display = 'none';
+      this.ticketShow[num].style.display = 'block';
+      this.index = num;
     }
-    // ------------------------------ Click NEXT SHOW ---------------------------------------
-    if (num === 3) {
-      this.ticketShow[(num - 1)].style.display = 'none';
-      return;
+
+    chooseTicket(num: number) {
+      this.isTwoWay = num;
     }
-    this.ticketShow[this.index].style.display = 'none';
-    this.ticketShow[num].style.display = 'block';
-    this.index = num;
-  }
 
-  chooseTicket(num: number) {
-    this.isTwoWay = num;
-  }
+    //#endregion
 
-  //#endregion
-
-  getAllLocation() {
-    this.ticketHomeService.getAllLocations().subscribe(data => {
-      if (!data) {
-        return;
-      }
-      this.locationList = data;
-    });
-  }
-
-  getTop10cheapestFlights() {
-    this.ticketHomeService.getTop10cheapestFlights().subscribe(data => {
-      this.top10cheapestFlight = data;
-    });
-  }
-
-  chooseLocation(cityName: string) {
-    if (!this.locationFrom) {
-      this.locationFrom = cityName;
-    } else if (!this.locationTo && this.locationFrom !== cityName) {
-      this.locationTo = cityName;
-    } else {
-      return;
-    }
-  }
-
-  //#region GET QUANTITY PASSENGER
-  addPassenger(passenger: string) {
-    if (this.adults >= 9 && passenger === 'adults' ||
-      this.children >= this.adults && passenger === 'children' ||
-      this.baby >= this.adults && passenger === 'baby') {
-      return;
-    }
-    switch (passenger) {
-      case 'adults':
-        this.adults++;
-        break;
-      case 'children':
-        this.children++;
-        break;
-      case 'baby':
-        this.baby++;
-        break;
-      default:
-        console.log('Bớt hack lại');
-    }
-  }
-
-  subPassenger(passenger: string) {
-    console.log(passenger);
-    console.log(this.adults);
-    if (this.adults <= 1 && passenger === 'adults' ||
-      this.children <= 0 && passenger === 'children' ||
-      this.baby <= 0 && passenger === 'baby') {
-      return;
-    }
-    switch (passenger) {
-      case 'adults':
-        if (this.adults === this.children) {
-          this.children--;
+    getAllLocation() {
+      this.ticketHomeService.getAllLocations().subscribe(data => {
+        if (!data) {
+          return;
         }
-        if (this.adults === this.baby) {
-          this.baby--;
-        }
-        this.adults--;
-        break;
-      case 'children':
-        this.children--;
-        break;
-      case 'baby':
-        this.baby--;
-        break;
-      default:
-        console.log('Bớt hack lại');
-    }
-  }
-
-  //#endregion
-
-  getSearchTicket() {
-    if ((this.isTwoWay && (!this.locationTo || !this.locationFrom || !this.endTime)) ||
-      (!this.isTwoWay && (!this.locationTo || !this.locationFrom))) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Xảy ra lỗi',
-        text: 'Vui lòng điền đầy đủ thông tin!'
+        this.locationList = data;
       });
-      return;
     }
-    this.searchTicket = {
-      locationTo: this.locationTo,
-      locationFrom: this.locationFrom,
-      departureTime: this.departureTime,
-      endTime: this.endTime,
-      passenger: this.adults + ',' + this.children + ',' + this.baby
-    };
-    this.router.navigateByUrl('/test', {state: this.searchTicket});
-  }
+
+    getTop10cheapestFlights() {
+      this.ticketHomeService.getTop10cheapestFlights().subscribe(data => {
+        this.top10cheapestFlight = data;
+      });
+    }
+
+    chooseLocation(cityName: string) {
+      if (!this.locationFrom) {
+        this.locationFrom = cityName;
+      } else if (!this.locationTo && this.locationFrom !== cityName) {
+        this.locationTo = cityName;
+      } else {
+        return;
+      }
+    }
+
+    //#region GET QUANTITY PASSENGER
+    addPassenger(passenger: string) {
+      if (this.adults >= 9 && passenger === 'adults' ||
+        this.children >= this.adults && passenger === 'children' ) {
+        return;
+      }
+      switch (passenger) {
+        case 'adults':
+          this.adults++;
+          break;
+        case 'children':
+          this.children++;
+          break;
+        default:
+          console.log('Bớt hack lại');
+      }
+    }
+
+    subPassenger(passenger: string) {
+      console.log(passenger);
+      console.log(this.adults);
+      if (this.adults <= 1 && passenger === 'adults' ||
+        this.children <= 0 && passenger === 'children') {
+        return;
+      }
+      switch (passenger) {
+        case 'adults':
+          if (this.adults === this.children) {
+            this.children--;
+          }
+          this.adults--;
+          break;
+        case 'children':
+          this.children--;
+          break;
+        default:
+          console.log('Bớt hack lại');
+      }
+    }
+
+    //#endregion
+
+    getSearchTicket() {
+      if ((this.isTwoWay && (!this.locationTo || !this.locationFrom || !this.endDay)) ||
+        (!this.isTwoWay && (!this.locationTo || !this.locationFrom))) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Xảy ra lỗi',
+          text: 'Vui lòng điền đầy đủ thông tin!'
+        });
+        return;
+      }
+      this.searchTicket = {
+        locationTo: this.locationTo,
+        locationFrom: this.locationFrom,
+        departureTime: this.startDay,
+        endTime: this.endDay,
+        passenger: this.adults + ',' + this.children
+      };
+      this.router.navigateByUrl('/test', {state: this.searchTicket});
+    }
 
   chosenFlight(flightId: number) {
     this.isTwoWay = 0;
@@ -177,7 +166,7 @@ export class HomeComponent implements OnInit {
       if (item.flightId === flightId) {
         this.locationFrom = item.locationFromName;
         this.locationTo = item.locationToName;
-        this.departureTime = item.flightDate;
+        this.startDay = item.flightDate;
       }
     });
     window.scrollTo({
